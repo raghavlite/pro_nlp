a3b700f019d010078042da9fee0469ad4f877f86
raghavlite <raghavlite@gmail.com>
2015-10-11 00:30:21 +0530

    done with coding part

--- pre_process.py
+++ pre_process.py
@@ -1,47 +1,83 @@
 from bs4 import *
 import re
-
-
-txt_again = open('./S5CV0001P0.xml','r');
-txt_again = txt_again.read();
-txt_again = re.sub('<col>\d+</col>','',txt_again);
-
-
-
-
-soup = BeautifulSoup(txt_again);
-
-# /////get year////////////////////
-jj = ' '.join([str(s.extract()) for s in soup('titlepage')]);
-jj = BeautifulSoup(jj);
-jj = jj.get_text();
-
-year = re.search('(\d{4})',jj);
-if(year!=None):
-	year = int(year.groups()[0]);
-else:
-	print 'mango dolly';
+import os
 
 
 
 
 
-kk = ' '.join([str(s.extract()) for s in soup('housecommons')]);
-ll = ' '.join([str(s.extract()) for s in soup('houselords')]);
-mm = ' '.join([str(s.extract()) for s in soup('writtenanswers')]);
+
+f_list = os.listdir("./")
+
+
+
+for km in f_list: 
+
+	print km;
+	txt_again = open('./'+km,'r');
+	txt_again = txt_again.read();
+	txt_again = re.sub('<col>\d+</col>','',txt_again);
+	txt_again = re.sub('&#......',' ',txt_again)
+	# &#x2014;
+	# &#x00E2;
+
+
+	soup = BeautifulSoup(txt_again);
+
+	# /////get year////////////////////
+	jj = ' '.join([str(s.extract()) for s in soup('titlepage')]);
+	jj = BeautifulSoup(jj);
+	jj = jj.get_text();
+
+	year = re.search('(\d{4})',jj);
+	if(year!=None):
+		year = int(year.groups()[0]);
+	else:
+		print 'mango dolly';
 
 
 
 
 
-kk = BeautifulSoup(kk+'\n'+ll+'\n'+mm);
-
-pp = ' '.join([str(s.extract()) for s in kk('p')]);
-
-
-pp = BeautifulSoup(pp);
+	kk = ' '.join([str(s.extract()) for s in soup('housecommons')]);
+	ll = ' '.join([str(s.extract()) for s in soup('houselords')]);
+	mm = ' '.join([str(s.extract()) for s in soup('writtenanswers')]);
 
 
-pp = pp.get_text();
+
+
+
+	kk = BeautifulSoup(kk+''+ll+''+mm);
+	pp = ' '.join([str(s.extract()) for s in kk('p')]);
+
+
+
+	pp = BeautifulSoup(pp);
+	pp = pp.get_text();
+	pp = re.sub('\r','',pp)
+	pp = re.sub('\n','',pp)
+	pp = re.sub('\n\n','',pp)
+
+
+
+
+
+
+	# pp = pp.rstrip();
+
+	f = open('./'+str((year/10)*10)+'.txt','a');
+	f.write(pp);
+	f.close();
+'end for'
+
+# txt_again = open('./'+str((year/10)*10)+'.txt','r');
+# txt_again = txt_again.read();
+# pp = re.sub('\r','',txt_again)
+# pp = pp.rstrip();
+
+# f = open('./'+str((year/10)*10)+'.txt','w');
+# f.write(pp);
+# f.close();
+
 
 
